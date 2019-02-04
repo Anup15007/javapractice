9be1f77b3d0cd215a5db385db30842cb83619e59
Anup15007 <47214732+Anup15007@users.noreply.github.com>
2019-02-04 08:58:33 +0530

    Add files via upload

---
+++ WrapperClassDemo/src/MainClass.java
@@ -0,0 +1,12 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	int num1=10;
+	int num2=5;
+	Integer iob=num2;
+	Integer iob1=num1+iob+iob+num2;
+	
+	System.out.println("value of iob:"+iob1);
+	}
+}
