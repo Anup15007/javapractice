1d84dd024143e3408e70ea70405306deaea8d910
Anup15007 <47214732+Anup15007@users.noreply.github.com>
2019-02-04 08:58:52 +0530

    Add files via upload

---
+++ InterfaceDemo/src/com/cg/mathproject/services/MathServicesImpl.java
@@ -0,0 +1,30 @@
+package com.cg.mathproject.services;
+
+public class MathServicesImpl  implements MathServices{
+
+	@Override
+	public int add(int n1, int n2) {
+	
+		return 0;
+	}
+
+	@Override
+	public int sub(int n1, int n2) {
+		
+		return 0;
+	}
+
+	@Override
+	public int div(int n1, int n2) {
+		
+		return 0;
+	}
+
+	@Override
+	public int muilt(int n1, int n2) {
+		
+		return 0;
+	}
+	
+
+}
